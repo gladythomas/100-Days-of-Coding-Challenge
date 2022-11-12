152255954c532630d33039566caa541cc90b5354
Glady Thomas <97222269+gladythomas@users.noreply.github.com>
2022-11-12 22:17:58 +0530

    Concatenation of Array.

---
+++ Day 08/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        // ArrayList<Integer> A=new ArrayList<Integer>();
+        int [] arr=new int[nums.length*2];
+        
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            arr[i+nums.length]=nums[i];
+        }
+      return arr;  
+}
+}
